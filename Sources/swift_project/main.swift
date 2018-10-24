a327e4c7a417b9913bf93a2aa35ab20f75e6265b
James Kwok <james.kwok@siteminder.com>
2018-10-25 10:54:42 +1100

    dispatch queue WIP

--- Sources/swift_project/main.swift
+++ Sources/swift_project/main.swift
@@ -1,9 +1,26 @@
+import Foundation
 import Alamofire
-  print("Calling...")
-  Alamofire.request( "http://httpbin.org/get", parameters: ["foo": "bar"])
-  .responseJSON   { response in
-    print("Hello, world!")
-    print(response)
-  }
-  print("Done!!")
 
+print("Helloworld")
+
+let queue = DispatchQueue(label: "com.cnoon.response-queue", qos: .utility, attributes: [.concurrent])
+
+Alamofire.request("http://httpbin.org/get", parameters: ["foo": "bar"])
+    .response(
+        queue: queue,
+        responseSerializer: DataRequest.jsonResponseSerializer(),
+        completionHandler: { response in
+            // You are now running on the concurrent `queue` you created earlier.
+            print("Parsing JSON on thread: \(Thread.current) is main thread: \(Thread.isMainThread)")
+
+            // Validate your JSON response and convert into model objects if necessary
+            print(response.result.value)
+
+            // To update anything on the main thread, just jump back on like so.
+            DispatchQueue.main.async {
+                print("Am I back on the main thread: \(Thread.isMainThread)")
+            }
+        }
+    )
+
+    sleep(5)
