e71511fd0ba93b743341326ace1cd4c2c3c258ca
James Kwok <jameskwok@Jamess-MacBook-Pro.local>
2018-11-01 19:11:34 +0800

    closures

--- Sources/swift_project/main.swift
+++ Sources/swift_project/main.swift
@@ -1,8 +1,13 @@
 import Foundation
 import Alamofire
 
-print("Helloworld")
+// https://theswiftdev.com/2018/07/10/ultimate-grand-central-dispatch-tutorial-in-swift/
 
+// https://www.sitepoint.com/improve-swift-closures-result/
+
+print("invoking")
+
+var flag = false
 let queue = DispatchQueue(label: "com.cnoon.response-queue", qos: .utility, attributes: [.concurrent])
 
 Alamofire.request("http://httpbin.org/get", parameters: ["foo": "bar"])
@@ -10,17 +15,10 @@ Alamofire.request("http://httpbin.org/get", parameters: ["foo": "bar"])
         queue: queue,
         responseSerializer: DataRequest.jsonResponseSerializer(),
         completionHandler: { response in
-            // You are now running on the concurrent `queue` you created earlier.
-            print("Parsing JSON on thread: \(Thread.current) is main thread: \(Thread.isMainThread)")
-
-            // Validate your JSON response and convert into model objects if necessary
-            print(response.result.value)
-
-            // To update anything on the main thread, just jump back on like so.
-            DispatchQueue.main.async {
-                print("Am I back on the main thread: \(Thread.isMainThread)")
-            }
+            print("calling out")
+            flag = true
         }
     )
 
-    sleep(5)
+sleep(5)
+print(flag)
